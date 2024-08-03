9459283e94f7d456c5c05c761eddb174f799de59
PRAKASHZZ007 <prakashtgvcplayer60@gmail.com>
2024-08-03 16:22:23 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/property-rental-platform/', // Ensure this matches your GitHub repository name
   plugins: [react()],
-})
+});
